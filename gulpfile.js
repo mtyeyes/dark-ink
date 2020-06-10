73598eeba925acee7383f7e7db3a230bd99bb996
mtyeyes <mtyeyes@mail.ru>
2020-06-10 14:31:03 +0300

    Updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
-"use strict";
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const testServer = require("browser-sync").create();
+const csso = require("gulp-csso");
+const imagemin = require("gulp-imagemin");
+const htmlmin = require("gulp-htmlmin");
+const del = require("del");
+const concat = require("gulp-concat");
+const babel = require("gulp-babel");
+const terser = require("gulp-terser");
+const webp = require('gulp-webp');
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
-var csso = require("gulp-csso");
-var imagemin = require("gulp-imagemin");
-var htmlmin = require("gulp-htmlmin");
-var del = require("del");
-var concat = require("gulp-concat");
-var babel = require("gulp-babel");
-var terser = require("gulp-terser");
-var webp = require('gulp-webp');
-
-gulp.task("html", function () {
+const html = () => {
   return gulp.src("source/*.html")
   .pipe(htmlmin({ collapseWhitespace: true }))
   .pipe(gulp.dest("build"));
-});
+};
 
-gulp.task("css", function () {
+exports.html = html;
+
+const css = () => {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
@@ -30,10 +30,12 @@ gulp.task("css", function () {
     ]))
     .pipe(csso())
     .pipe(gulp.dest("build/css"))
-    .pipe(server.stream());
-});
+    .pipe(testServer.stream());
+};
 
-gulp.task("img", function () {
+exports.css = css;
+
+const img = () => {
   return gulp.src(["source/img/**/*.{png,jpg,svg}", "!source/img/gallery*uc.jpg}"])
   .pipe(imagemin([
     imagemin.optipng({optimizationLevel: 4}),
@@ -41,59 +43,79 @@ gulp.task("img", function () {
     imagemin.svgo()
   ]))
   .pipe(gulp.dest("source/img"));
-});
+};
 
-gulp.task("convert", function () {
+exports.img = img;
+
+const convert = () => {
   return gulp.src(["source/img/*.{png,jpg}", "!source/img/gallery*uc.jpg}", "!source/img/*-bg*.{png,jpg}"])
   .pipe(webp())
   .pipe(gulp.dest("source/img"));
-});
+};
 
-gulp.task("server", function () {
-  server.init({
+exports.convert = convert;
+
+const server = () => {
+  testServer.init({
     server: "build/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
+};
 
+exports.server = server;
+
+const watch = () => {
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/js/*", gulp.series("script", "es5script", "nonstrict", "refresh"));
-});
+};
 
-gulp.task("refresh", function (done) {
+exports.watch = watch;
+
+const refresh = () => {
   server.reload();
   done();
-});
+};
 
-gulp.task("clean", function (){
+exports.refresh = refresh;
+
+const clean = () => {
   return del("build");
-});
+};
 
-gulp.task("script", function() {
+exports.clean = clean;
+
+const script = () => {
   return gulp.src(["source/js/*.js"])
-    .pipe(concat("script.js"))
-    .pipe(terser())
-    .pipe(gulp.dest("build/js/"));
-});
+  .pipe(concat("script.js"))
+  .pipe(terser())
+  .pipe(gulp.dest("build/js/"));
+};
 
-gulp.task("nonstrict", function() {
+exports.script = script;
+
+const nonstrict = () => {
   return gulp.src(["source/js/non-strict/*.js"])
-    .pipe(terser())
-    .pipe(gulp.dest("build/js/"));
-});
+  .pipe(terser())
+  .pipe(gulp.dest("build/js/"));
+};
 
-gulp.task("es5script", function() {
+exports.nonstrict = nonstrict;
+
+const es5script = () => {
   return gulp.src(["node_modules/@babel/polyfill/dist/polyfill.js", "source/js/*.js"])
   .pipe(concat("script.es5.js"))
   .pipe(babel())
   .pipe(terser())
   .pipe(gulp.dest("build/js/"));
-})
+};
 
-gulp.task("copy", function (){
+exports.es5script = es5script;
+
+const copy = () => {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/*.*",
@@ -103,10 +125,40 @@ gulp.task("copy", function (){
     base: "source"
   })
   .pipe(gulp.dest("build"));
-});
+};
 
-gulp.task("images", gulp.series("img", "convert"))
+exports.copy = copy;
 
-gulp.task("build", gulp.series("clean", "copy", "css", "script", "es5script", "nonstrict", "html"));
+const images = gulp.series(img, convert);
 
-gulp.task("start", gulp.series("build", "server"));
+exports.images = images;
+
+const build = gulp.series(
+  clean,
+  gulp.parallel(
+    html,
+    copy,
+    css,
+    script,
+    es5script,
+    nonstrict,
+  ),
+);
+
+exports.build = build;
+
+exports.default = gulp.series(
+  clean,
+  gulp.parallel(
+    html,
+    copy,
+    css,
+    script,
+    es5script,
+    nonstrict,
+  ),
+  gulp.parallel(
+    server,
+    watch,
+  )
+);
