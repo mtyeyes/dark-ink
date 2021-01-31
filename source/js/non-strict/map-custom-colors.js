d820e493a215aeecbd4b89c4f8a1727d21ba9e0b
mtyeyes <mymagicbox@list.ru>
2021-01-31 15:30:00 +0300

    Google maps error fixed

--- source/js/non-strict/map-custom-colors.js
+++ source/js/non-strict/map-custom-colors.js
@@ -282,7 +282,7 @@ function initMap() {
 (function() {
   var apiKey = document.createElement("script");
   var apiKeyEncodedParts = ['9Cy34_', 'Tqe-oq0', 'PWubln4g', 'mre2rlMQm', 'AIzaSyBkE'];
-  apiKey.src = 'https://maps.googleapis.com/maps/api/js?key=' + window.atob((apiKeyEncodedParts[4] + apiKeyEncodedParts[1] + apiKeyEncodedParts[0] + apiKeyEncodedParts[3] + apiKeyEncodedParts[2])) + '&callback=initMap';
+  apiKey.src = 'https://maps.googleapis.com/maps/api/js?key=' + (apiKeyEncodedParts[4] + apiKeyEncodedParts[1] + apiKeyEncodedParts[0] + apiKeyEncodedParts[3] + apiKeyEncodedParts[2]) + '&callback=initMap';
   apiKey.type = "text/javascript";
   apiKey.setAttribute("defer", "defer");
   document.body.appendChild(apiKey);
